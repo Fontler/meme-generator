5fd47db3759806febf6ef72594b6a84111182af5
Alexander Fontler <mallexthewise@gmail.com>
2023-08-02 21:23:15 +0200

    Static assets ready

---
+++ src/components/Meme.jsx
@@ -0,0 +1,14 @@
+import './Meme.scss'
+
+export default function() {
+    
+    return (
+        <main>
+            <form>
+                <input type="text" placeholder="Top text" />
+                <input type="text" placeholder="Bottom text" />
+                <button>Get a new meme image 🖼️</button>
+            </form>
+        </main>
+    )
+}
