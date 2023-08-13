c7928298456c969535d571cb82838e627a4018ec
Alexander Fontler <mallexthewise@gmail.com>
2023-08-13 23:47:59 +0200

    Finishied project

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
-import memesData from '../memesData'
+import { useState, useEffect } from 'react'
 import './Meme.scss'
 
 export default function Meme() {
     const [meme, setMeme] = useState({
-        topText: 'One does not simply',
-        bottomText: 'Walk into Mordor',
+        topText: 'Top text',
+        bottomText: 'Bottom text',
         randomImage: 'http://i.imgflip.com/1bij.jpg',
     })
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [allMemes, setAllMeme] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+            .then(res => res.json())
+            .then(resData => setAllMeme(resData.data.memes))
+    }, [])
 
     function getMemeImage(e) {
         e.preventDefault()
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
     }
+
     function handleChange(event) {
         const {name, value} = event.target
         setMeme(prevChange => ({
@@ -27,6 +32,7 @@ export default function Meme() {
             [name]: value
         }))
     }
+
     return (
         <main>
             <form>
